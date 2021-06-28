826cb52d421410efb74d76d1b04b442b90417b18
Minh Tran <mctran98@yahoo.com>
2021-06-28 13:42:51 -0700

    Displayed template create account form

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -1,8 +1,14 @@
 import Head from 'next/head';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import styles from 'src/styles/create_account.module.scss';
+import Image from 'next/image';
+import wealthfront from '../images/wealthfront.svg';
 
 export default function CreateAccount() {
+  const [username, setUsername] = useState('');
+  const [firstPassword, setFirstPassword] = useState('');
+  const [SecondPassword, setSecondPassword] = useState('');
+
   async function handleSubmit(evt: FormEvent) {
     evt.preventDefault();
     const response = await fetch('/api/create_new_account', {
@@ -11,7 +17,19 @@ export default function CreateAccount() {
     });
 
     console.log(await response.json());
-  }
+  };
+
+  var handleUsername = user => {
+    setUsername(user);
+  };
+
+  var handleFirstPassword = pass => {
+    setFirstPassword(pass);
+  };
+
+  var handleSecondPassword = pass => {
+    setSecondPassword(pass);
+  };
 
   return (
     <>
@@ -20,6 +38,20 @@ export default function CreateAccount() {
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
+          <Image src={wealthfront} />
+          <h2>Create New Account</h2>
+          <div>
+            <label>Username</label>
+            <input type='input' onChange={e => handleUsername(e.target.value)}></input>
+          </div>
+          <div>
+            <label>Password</label>
+            <input type='password' onChange={e => handleFirstPassword(e.target.value)}></input>
+          </div>
+          <div>
+            <label>Re-enter password</label>
+            <input type='password' onChange={e => handleSecondPassword(e.target.value)}></input>
+          </div>
           <button>Create Account</button>
         </form>
       </article>
