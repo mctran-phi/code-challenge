6ce84dbf24a67a297cde5cb959e39478d333b37b
Minh Tran <mctran98@yahoo.com>
2021-06-28 14:31:51 -0700

    Styled form

--- src/pages/create_account.tsx
+++ src/pages/create_account.tsx
@@ -35,24 +35,41 @@ export default function CreateAccount() {
     <>
       <Head>
         <title>Create Account</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;900&display=swap" rel="stylesheet" />
       </Head>
       <article className={styles.article}>
         <form className={styles.form} onSubmit={handleSubmit}>
-          <Image src={wealthfront} />
-          <h2>Create New Account</h2>
-          <div>
-            <label>Username</label>
-            <input type='input' onChange={e => handleUsername(e.target.value)}></input>
+          <div className={styles.image}>
+            <Image src={wealthfront} />
           </div>
-          <div>
-            <label>Password</label>
-            <input type='password' onChange={e => handleFirstPassword(e.target.value)}></input>
+          <h1 className={styles.header}>Create New Account</h1>
+          <div className={styles.input_container}>
+            <label className={styles.label}>Username</label>
+            <input
+              className={styles.input}
+              type='input'
+              onChange={e => handleUsername(e.target.value)}
+            />
           </div>
-          <div>
-            <label>Re-enter password</label>
-            <input type='password' onChange={e => handleSecondPassword(e.target.value)}></input>
+          <div className={styles.input_container}>
+            <label className={styles.label}>Password</label>
+            <input
+              className={styles.input}
+              type='password'
+              onChange={e => handleFirstPassword(e.target.value)}
+            />
           </div>
-          <button>Create Account</button>
+          <div className={styles.input_container}>
+            <label className={styles.label}>Re-enter password</label>
+            <input
+              className={styles.input}
+              type='password'
+              onChange={e => handleSecondPassword(e.target.value)}
+            />
+          </div>
+          <button className={styles.button}>Create Account</button>
         </form>
       </article>
     </>
