416d3d7648b984958595b714644e346818b7f162
Minh Tran <mctran98@yahoo.com>
2021-06-28 16:29:10 -0700

    Fetched result from password_exposed

--- src/pages/api/create_new_account.ts
+++ src/pages/api/create_new_account.ts
@@ -10,6 +10,29 @@ interface BooleanResult {
   errors?: Record<string, string>;
 }
 
-export default function createNewAccount(req: NextApiRequest, res: NextApiResponse<BooleanResult>) {
+export default async function createNewAccount(req: NextApiRequest, res: NextApiResponse<BooleanResult>) {
+  const { username, password}: CreateNewAccountParameters = req.body;
+
+  const response = await fetch('/api/create_new_account', {
+    method: 'POST',
+    body: JSON.stringify(password),
+  });
+  const { result } = await response.json();
+
+  if (!result) {
+    res.status(200).json({
+      result: false,
+      errors: {error: 'exposed'}
+    });
+  }
+
+  var userLength = /.{10,50}/;
+  var validUser = /^([a-zA-z0-9]){10,50}$/;
+  var passLength = /.{20,50}/;
+  var passAtLeastOne = /([a-zA-Z])+([0-9])+([!@#$%])+/
+  var validPass = /^([a-zA-Z0-9!@#$%]){20,50}$/;
+  if (userLength.test(req.body.username)) {
+
+  }
   res.status(200).json({ result: true });
 }
